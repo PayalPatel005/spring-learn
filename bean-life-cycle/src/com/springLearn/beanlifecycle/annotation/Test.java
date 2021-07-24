346865512f5abbd4abe0826d921ee3dcb5169670
Payal Patel <202012121@daiict.ac.in>
2021-07-25 00:04:42 +0530

    registerShutdownHook added

--- bean-life-cycle/src/com/springLearn/beanlifecycle/annotation/Test.java
+++ bean-life-cycle/src/com/springLearn/beanlifecycle/annotation/Test.java
@@ -7,9 +7,11 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Test {
 	public static void main(String[] args) throws ClassNotFoundException, SQLException {
-		ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
 		StudentDAO dao = context.getBean("studentDAO", StudentDAO.class);
 		dao.selectAllRows();
-		((ClassPathXmlApplicationContext)context).close();
+		context.close();
+		context.registerShutdownHook(); // same as close method but it'll execute only 
+		// once when the main thread ends. Irrespective of the line number we're calling it
 	}
 }
